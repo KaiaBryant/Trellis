9c1362a9cb6b4ac4e7dda464642326c62a95a82f
Kaia <kaia.bryant@yahoo.com>
2025-08-19 12:12:31 -0400

    edited JS contact form

--- contact/contact.js
+++ contact/contact.js
@@ -1,34 +1,36 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const form = document.querySelector('.contact-form form');
-//     // console.log("script loaded.");
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.contact-form');
+    // console.log("script loaded.");
 
-//     form.addEventListener('submit', function (e) {
-//         e.preventDefault();
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
 
-//         // const firstName = document.getElementById('first-name');
-//         // const lastName = document.getElementById('last-name');
-//         const email = document.getElementById('email');
-//         const phone = document.getElementById('phone');
-//         // const seed = document.getElementById('seeds');
+        // const firstName = document.getElementById('first-name');
+        // const lastName = document.getElementById('last-name');
+        const email = document.getElementById('email');
+        const phone = document.getElementById('phone');
+        // const seed = document.getElementById('seeds');
 
-//         let errors = [];
+        let errors = [];
 
-//         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//         if (!emailPattern.test(email.value)) {
-//             errors.push("Enter a valid email address.");
-//         }
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailPattern.test(email.value)) {
+            errors.push("Enter a valid email address.");
+        }
 
-//         const phonePattern = /^\+?(\d{1,3})?[\s.-]?\(?(\d{3})\)?[\s.-]?(\d{3})[\s.-]?(\d{4})$/;
-//         if (phone.value && !phonePattern.test(phone.value)) {
-//             errors.push("Phone number must be in 123-456-7890.");
-//         }
+        const phonePattern = /^\+?(\d{1,3})?[\s.-]?\(?(\d{3})\)?[\s.-]?(\d{3})[\s.-]?(\d{4})$/;
+        if (phone.value && !phonePattern.test(phone.value)) {
+            errors.push("Phone number must be in 123-456-7890.");
+        }
 
-//         if (errors.length > 0) {
-//             alert(errors.join("\n"));
-//         } else {
-//             form.submit();
-//         }
-//     });
+        if (errors.length > 0) {
+            alert(errors.join("\n"));
+        } else {
+            alert("Form submitted successfully!");
+            form.reset();
+        }
+        console.log(errors);
+    });
 
-// });
+});
 
